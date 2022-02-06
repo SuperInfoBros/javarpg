08a4a4cb88676f915cd4fd289782e34eb74cb8c9
Raoul1808 <37770353+Raoul1808@users.noreply.github.com>
2022-02-06 12:18:46 +0100

    Projet de base

---
+++ desktop/src/io/superinfobros/javarpg/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.superinfobros.javarpg.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.superinfobros.javarpg.JavaRPGGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new JavaRPGGame(), config);
+	}
+}
