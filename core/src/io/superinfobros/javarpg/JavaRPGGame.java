0d05b428bd5f020b28a889e2249762e7d8bb32d2
Raoul1808 <37770353+Raoul1808@users.noreply.github.com>
2022-02-06 12:36:37 +0100

    Change clear color

--- core/src/io/superinfobros/javarpg/JavaRPGGame.java
+++ core/src/io/superinfobros/javarpg/JavaRPGGame.java
@@ -1,6 +1,7 @@
 package io.superinfobros.javarpg;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -8,6 +9,7 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class JavaRPGGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Color clearColor = Color.valueOf("6495EDFF");
 	
 	@Override
 	public void create () {
@@ -17,7 +19,7 @@ public class JavaRPGGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(clearColor.r, clearColor.g, clearColor.b, clearColor.a);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
